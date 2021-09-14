989cdf824ee44f7975653bd86f5612d8560623bf
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-09-14 16:14:35 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .container
+    h1 {{ welcome }}
+</template>
+
+
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {
+      welcome: 'Всем приветик)))',
+    }
+  },
+})
+</script>
